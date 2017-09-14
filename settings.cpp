5ef3dcbcaa9fc3e58d597947f1938d4be7bc7dcd
FunkyLoiso <funkyloiso@gmail.com>
2017-09-14 14:38:30 +0300

    log-path and log-level options added

--- settings.cpp
+++ settings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 #include "settings.hpp"
 
 namespace po = boost::program_options;
@@ -7,12 +8,37 @@ namespace po = boost::program_options;
 namespace sp
 {
 
+namespace str {
+namespace severity {
+  using namespace boost::log::trivial;
+  static const std::string trace(to_string(severity_level::trace));
+  static const std::string debug(to_string(severity_level::debug));
+  static const std::string info(to_string(severity_level::info));
+  static const std::string warning(to_string(severity_level::warning));
+  static const std::string error(to_string(severity_level::error));
+  static const std::string fatal(to_string(severity_level::fatal));
+}
+}
+
 namespace def {
+  static const auto log_level = str::severity::warning;
 }
 
 void describe(po::options_description& desc, settings* st = nullptr) {
+  auto severity_handler = [st](const std::string sv) {
+    if(!st) return;
+    if(boost::iequals(sv, str::severity::trace)) st->log_level = boost::log::trivial::trace;
+    else if(boost::iequals(sv, str::severity::debug)) st->log_level = boost::log::trivial::debug;
+    else if(boost::iequals(sv, str::severity::info)) st->log_level = boost::log::trivial::info;
+    else if(boost::iequals(sv, str::severity::warning)) st->log_level = boost::log::trivial::warning;
+    else if(boost::iequals(sv, str::severity::error)) st->log_level = boost::log::trivial::error;
+    else if(boost::iequals(sv, str::severity::fatal)) st->log_level = boost::log::trivial::fatal;
+  };
+
   desc.add_options()
     ("help,h", "print this message and exit")
+    ("log-path,L", po::value<std::string>(st ? &st->log_path : nullptr), "full path to log file. syslog if absent")
+    ("log-level,l", po::value<std::string>()->notifier(severity_handler)->default_value(def::log_level), "log level: trace, debug, info, warning, error or fatal")
     ("pid-path,p", po::value<std::string>(st ? &st->pid_path : nullptr), "full path to pid file");
 }
 
--- settings.hpp
+++ settings.hpp
@@ -1,6 +1,7 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
 
+#include <boost/log/trivial.hpp>
 #include "exception.hpp"
 
 namespace sp {
@@ -16,8 +17,9 @@ public:
   void read(int argc, char* argv[]); // throws exception, help_requested
   void clear();
 
+  std::string log_path; // syslog if empty
+  boost::log::trivial::severity_level log_level; // trace, debug, info, warning, error, fatal
   std::string pid_path;
-  // ...
 };
 
 }
