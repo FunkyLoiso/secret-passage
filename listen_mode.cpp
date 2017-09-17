c2ac16c770af6d68d4d8c011037c16eac0094288
FunkyLoiso <funkyloiso@gmail.com>
2017-09-17 19:27:06 +0300

    implemented reading some data after accepting connection

--- listen_mode.cpp
+++ listen_mode.cpp
@@ -2,12 +2,26 @@
 #include <boost/bind.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/placeholders.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/buffers_iterator.hpp>
 #include "listen_mode.hpp"
 #include "logging.hpp"
 #include "normal_socket.hpp"
 #include "secure_socket.hpp"
 
 namespace asio = boost::asio;
+
+namespace
+{
+
+std::string buffer_to_string(const boost::asio::streambuf &buffer)
+{
+  auto bufs = buffer.data();
+  std::string result(asio::buffers_begin(bufs), asio::buffers_end(bufs));
+  return result;
+}
+
+}
 
 namespace sp
 {
@@ -18,11 +32,15 @@ namespace sp
 class listen_mode::private_t {
 public:
   private_t(boost::asio::io_service& ios, const settings& st, shared_descriptor tap);
+  ~private_t();
   void setup();
 
 private:
+  void close_and_listen();
   void async_accept();
   void handle_accept(const boost::system::error_code& ec);
+  void async_read();
+  void handle_read(const boost::system::error_code& ec, std::size_t tr);
 
   boost::asio::io_service& ios_;
   const settings& st_;
@@ -31,13 +49,34 @@ private:
   socket::acceptor acceptor_;
   socket::acceptor::endpoint_type remote_ep_;
   boost::shared_ptr<socket> socket_;
+
+  struct state {
+    enum code_t {
+      no_connection,
+      accepting_header,
+      accepting_data
+    };
+  };
+  state::code_t state_;
+
+  asio::streambuf buf_;
 };
 
 listen_mode::private_t::private_t(boost::asio::io_service& ios, const settings& st, shared_descriptor tap)
   : ios_(ios), st_(st), tap_(tap)
   , acceptor_(ios_)
+  , state_(state::no_connection)
 {
   socket_ = boost::make_shared<normal_socket>(ios_); // @TODO: read tls option from settings
+}
+
+listen_mode::private_t::~private_t() {
+  boost::system::error_code close_ec;
+  socket_->shutdown(close_ec);
+  if(close_ec) {
+    LOG_WARNING << bf("listen_mode dtor: error while shutting socket down after accept failure: %s")
+      % close_ec.message();
+  }
 }
 
 void listen_mode::private_t::setup() {
@@ -68,7 +107,7 @@ void listen_mode::private_t::setup() {
   auto ep = i->endpoint();
   LOG_DEBUG << bf("%s: resolved to '%s:%d'") % func % ep.address().to_string() % ep.port();
   acceptor_.open(ep.protocol());
-//  acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+  acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
   acceptor_.bind(ep, ec);
   if(ec) {
     throw exception(boost::str(bf("Failed to bind acceptor to endpont '%s:%s': %s")
@@ -82,6 +121,24 @@ void listen_mode::private_t::setup() {
     ));
   }
   async_accept();
+}
+
+void listen_mode::private_t::close_and_listen() {
+  static const char func[] = "listen_mode::close_and_listen";
+  boost::system::error_code close_ec;
+  socket_->shutdown(close_ec);
+  if(close_ec) {
+    LOG_WARNING << bf("%s: error while shutting socket down after accept failure: %s")
+      % func % close_ec.message();
+  }
+  socket_->close(close_ec);
+  if(close_ec) {
+    LOG_WARNING << bf("%s: error while closing socket after accept failure: %s")
+      % func % close_ec.message();
+  }
+  acceptor_.cancel();
+  async_accept();
+  return;
 }
 
 void listen_mode::private_t::async_accept() {
@@ -104,23 +161,51 @@ void listen_mode::private_t::handle_accept(const boost::system::error_code& ec) 
 
   if(ec) {
     LOG_ERROR << bf("%s: accept opration failed, retrying: %s") % func % ec.message();
-    boost::system::error_code close_ec;
-    socket_->shutdown(close_ec);
-    if(close_ec) {
-      LOG_WARNING << bf("%s: error while shutting socket down after accept failure: %s")
-        % func % close_ec.message();
-    }
-    socket_->close(close_ec);
-    if(close_ec) {
-      LOG_WARNING << bf("%s: error while closing socket after accept failure: %s")
-        % func % close_ec.message();
-    }
-    async_accept();
+    close_and_listen();
     return;
   }
 
-  LOG_DEBUG << bf("%s: accepted connection from '%s:%d'")
+  LOG_INFO << bf("%s: accepted connection from '%s:%d', awaiting request..")
     % func % remote_ep_.address().to_string() % remote_ep_.port();
+  state_ = state::accepting_header;
+  async_read();
+}
+
+void listen_mode::private_t::async_read() {
+  socket_->async_read_some(
+    buf_.prepare(512),
+    boost::bind(
+      &private_t::handle_read,
+        this,
+        asio::placeholders::error,
+        asio::placeholders::bytes_transferred
+    )
+  );
+}
+
+void listen_mode::private_t::handle_read(const boost::system::error_code& ec, std::size_t tr) {
+  static const char func[] = "listen_mode::handle_read";
+  if(asio::error::operation_aborted == ec) {
+    LOG_TRACE << func << ": operation aborted";
+    return;
+  }
+
+  // @TODO: handle close errors differently?
+  if(ec) {
+    LOG_WARNING << bf("%s: read operation error, closing socket and accepting again: %s")
+      % func % ec.message();
+    close_and_listen();
+    return;
+  }
+
+  // actually some data!
+  buf_.commit(tr);
+  if(state::accepting_header == state_) {
+    LOG_DEBUG << bf("%s: received request data (%d bytes):\n%s")
+      % func % tr % buffer_to_string(buf_);
+  }
+  // @TODO: parse http
+  async_read();
 }
 
 /*\
